09575ca3b0fd00fa74c128261905757e9a4cb3c6
Swapnil Kelkar <skelkar@purdue.edu>
2020-06-08 21:48:57 -0400

    Updates

---
+++ precache-manifest.bb8368beed8a8cc98a87973bbdd61c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a6dc02f08a244a0ed0b090b459be82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d68a2ed5043251e6f3bf",
+    "url": "/monsters-rolodex/static/css/main.603c4587.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68a2ed5043251e6f3bf",
+    "url": "/monsters-rolodex/static/js/main.e8a878b9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
